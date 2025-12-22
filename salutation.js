62751c33fec70d3600e750b30200813f3e642a09
Baidda <ton-emailexemple.com>
2025-12-22 17:41:02 +0100

    Ajout fonction pour ajouter un utilisateur

--- salutation.js
+++ salutation.js
@@ -11,6 +11,16 @@ function saluerTous() {
 console.log("=================="); 
 }
 
+
+// Ajoute cette fonction à la fin de salutations.js 
+function ajouterUtilisateur(nom) { 
+	Guide clonage  d’un dépôt distant -   DD2web- M. TAIS 
+	utilisateurs.push(nom); 
+	console.log(`${nom} a été ajouté à la liste !`); 
+} 
+// Tester la nouvelle fonction 
+ajouterUtilisateur("Émilie"); 
+saluerTous(); 
 // Exécuter 
 
 saluerTous(); 
